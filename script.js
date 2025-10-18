568f3b9b800f9f66e4b69ec622b7204354bbed6f
Benjamin Blarr <b.blarr@gmx.de>
2025-10-18 12:47:14 +0200

    Changed burger menu and made it responsible

--- script.js
+++ script.js
@@ -18,17 +18,17 @@ function sendMail(event) {
 }
 
 function toggleMenu() {
-  document.getElementById("menu").classList.toggle("d_none");
+  document.getElementById("menu").classList.toggle("is-open");
 }
 
 fetch("footer.html")
-  .then(response => response.text())
-  .then(data => {
+  .then((response) => response.text())
+  .then((data) => {
     document.getElementById("footer").innerHTML = data;
   });
 
 fetch("header.html")
-  .then(response => response.text())
-  .then(data => {
+  .then((response) => response.text())
+  .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
